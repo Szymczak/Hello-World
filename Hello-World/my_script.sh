58d531d0deed12e252964d9c1f484054a1457805
Pawel Szymczak <p.szymczak.0@gmail.com>
2011-06-14 13:17:20 +0100

    Instrumentation change

--- Hello-World/my_script.sh
+++ Hello-World/my_script.sh
@@ -3,7 +3,7 @@ ant
 echo "PROJECT HAS BEEN BUILT"
 adb install bin/HelloWorld.apk
 echo "PROJECT HAS BEEN INSTALLED"
-adb shell am instrument -w com.helloworld/android.test.InstrumentationTestRunner
+adb shell am instrument -w com.helloworld/com.helloworld.InstrumentationTestRunner
 echo "TESTS HAVE BEEN BUILT"
 adb uninstall com.helloworld
 echo "PROJECT HAS BEEN UNINSTALLED"
